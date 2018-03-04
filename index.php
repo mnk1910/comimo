ad6bb28729a6c198db4b2dde3759111f245de8be
mnk1910 <mnk1910@local>
2018-03-04 18:00:24 +0100

    Break into multiple pages

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include ("./includes/top.php");
+?>
+    <!-- Intro Header -->
+    <header class="masthead">
+      <div class="intro-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <h1 class="brand-heading">Grayscale</h1>
+              <p class="intro-text">A free, responsive, one page Bootstrap theme.
+                <br>Created by Start Bootstrap.</p>
+              <a href="./about.php" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+<?php
+include ("./includes/bottom.php");
+?>
+
